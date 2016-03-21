800bfe6931c9dd73cd0a56f3e4809cd2f79095fe
zliaky <xueai9984@126.com>
2016-03-21 10:15:06 +0800

    lec9

--- SPOC/lec9/page_fault_rate.cpp
+++ SPOC/lec9/page_fault_rate.cpp
@@ -2,39 +2,31 @@
 #include <stdio.h>
 using namespace std;
 
-#define T_MAX 10
+#define MAX_PAGE 100
+#define MAX_TIME 1000
 
-int working_set[100];
-bool refer[100];
-int size = 0;
+int working_set[MAX_PAGE];
+int max_page;
 int window_size;
-int history[1010];
-int t_current = -1;
-int t_last = -1;
-int max_page = 0;
-
-int find(int n)
-{
-	for (int i = 0; i < size; i++)
-	{
-		if (working_set[i] == n) return i;
-	}
-	return -1;
-}
+int history[MAX_TIME];
+int t_current;
+int t_last;
+int t;
+int error_t;
 
 void out()
 {
-	for (int i = 0; i < size; i++)
+	cout << "working set: ";
+	for (int i = 0; i <= max_page; i++)
 	{
-		cout << working_set[i] << " ";
+		if (working_set[i] == true) cout << i << " ";
 	}
 	cout << endl;
 }
 
 void add(int page)
 {
-	working_set[size] = page;
-	size++;
+	working_set[page] = true;
 }
 
 void replace(int page)
@@ -42,50 +34,59 @@ void replace(int page)
 	cout << "now replace" << endl;
 	for (int i = 0; i <= max_page; i++)
 	{
-		refer[i] =  false;
+		working_set[i] = false;
 	}
 	for (int i = t_current-1; i > t_last; i--)
 	{
-		refer[history[i]] = true;
-	}
-	for (int i = 0; i < size; i++)
-	{
-		if (refer[working_set[i]] == false)
-		{
-			for (int j = i; j < size-1; j++)
-			{
-				working_set[j] = working_set[j+1];
-			}
-			size--;
-		}
+		working_set[history[i]] = true;
 	}
 	add(page);
 }
 
+void init()
+{
+	for (int i = 0; i < MAX_PAGE; i++)
+	{
+		working_set[i] = false;
+	}
+	t_current = -1;
+	t_last = -1;
+	max_page = 0;
+	t = 0;
+	error_t = 0;
+}
+
+void output_result()
+{
+	cout << "===============" << endl;
+	cout << "result:" << endl;
+	cout << "Hits = " << t - error_t << endl;
+	cout << "Misses = " << error_t << endl;
+	cout << "Hit rate = " << (double)(t-error_t)/t << endl;
+}
+
 int main()
 {
-//	freopen("input.txt", "r", stdin);
-	int t = 0;
-	int history_size = 0;
 	cout << "Window size:";
 	cin >> window_size;
+	init();
 	while (1)
 	{
+		cout << "===============" << endl;
 		int page;
 		cout << "step " << t << "         ";
 		cout << "Page no:";
 		cin >> page;
 		if (page > max_page) max_page = page;
-		history[history_size] = page;
-		history_size++;
+		history[t] = page;
 		if (page < 0)
 		{
-			cout << "error page no\n";
+			output_result();
 			break;
 		}
-		int it = find(page);
-		if (it == -1)
+		if (working_set[page] == false)
 		{
+			error_t++;
 			t_current = t;
 			cout << "page fault!   current time:" << t_current << "     last time:" << t_last << "\n";
 			if (t_current-t_last<=window_size)
@@ -99,8 +100,7 @@ int main()
 			t_last = t;
 		}
 		out();
-		cout << "===============" << endl;
-		t++;	
+		t++;
 	}
 	
 	return 0;
