c4c266b8a5a0a5a4b8d83483c5c90ca50be54e7c
xuweijie <xuweijie1995@sina.cn>
2016-03-30 14:41:05 +0800

    lec12

--- SPOC/lec12/process-cpuio-homework.py
+++ SPOC/lec12/process-cpuio-homework.py
@@ -4,10 +4,17 @@ import sys
 from optparse import OptionParser
 import random
 
+# process switch behavior
+SCHED_SWITCH_ON_IO = 'SWITCH_ON_IO'
+
+# io finished behavior
+IO_RUN_LATER = 'IO_RUN_LATER'
+
 # process states
 STATE_RUNNING = 'RUNNING'
 STATE_READY = 'READY'
 STATE_DONE = 'DONE'
+STATE_WAIT = 'WAITING'
 
 # members of process structure
 PROC_CODE = 'code_'
@@ -18,12 +25,15 @@ PROC_STATE = 'proc_state_'
 # things a process can do
 DO_COMPUTE = 'cpu'
 DO_YIELD = 'yld'
-
+DO_IO = 'io'
 
 class scheduler:
-    def __init__(self):
+    def __init__(self, process_switch_behavior, io_done_behavior, io_length):
         # keep set of instructions for each of the processes
         self.proc_info = {}
+        self.process_switch_behavior = process_switch_behavior
+        self.io_done_behavior = io_done_behavior
+        self.io_length = io_length
         return
 
     def new_process(self):
@@ -38,18 +48,26 @@ class scheduler:
     def load(self, program_description):
         proc_id = self.new_process()
         tmp = program_description.split(':')
-        if len(tmp) != 2:
-            print 'Bad description (%s): Must be number <x:y>'
+        if len(tmp) != 3:
+            print 'Bad description (%s): Must be number <x:y:z>'
             print '  where X is the number of instructions'
-            print '  and Y is the percent change that an instruction is CPU not YIELD'
+            print '  and Y is the percent change that an instruction is YIELD'
+            print '  and Z is the percent change that an instruction is IO'
             exit(1)
 
-        num_instructions, chance_cpu = int(tmp[0]), float(tmp[1])/100.0
+        num_instructions, chance_yield, chance_io = int(tmp[0]), float(tmp[1])/100.0, float(tmp[2])/100.0
+        assert(chance_yield+chance_io<1)
+
+        #print "proc %d, num_instr %d, change_cpu %f" % (proc_id,num_instructions, chance_cpu)
         for i in range(num_instructions):
-            if random.random() < chance_cpu:
+            randnum=random.random();
+            if randnum < (1.0-chance_yield-chance_io):
                 self.proc_info[proc_id][PROC_CODE].append(DO_COMPUTE)
-            else:
+            elif randnum >= (1.0-chance_yield-chance_io) and randnum < (1.0-chance_io):
                 self.proc_info[proc_id][PROC_CODE].append(DO_YIELD)
+            else:
+                self.proc_info[proc_id][PROC_CODE].append(DO_IO)
+            #print "proc %d, instr idx %d, instr cxt %s" % (proc_id, i, self.proc_info[proc_id][PROC_CODE][i])
         return
 
     #change to READY STATE, the current proc's state should be expected
@@ -78,6 +96,14 @@ class scheduler:
 
         if self.proc_info[self.curr_proc][PROC_STATE] == expected:
             self.proc_info[self.curr_proc][PROC_STATE] = STATE_DONE
+        return
+
+    #change to WAIT STATE, the current proc's state should be expected
+    def move_to_wait(self, expected):
+        #YOUR CODE
+
+        if self.proc_info[self.curr_proc][PROC_STATE] == expected:
+            self.proc_info[self.curr_proc][PROC_STATE] = STATE_WAIT
         return
 
     #choose next proc using FIFO/FCFS scheduling, If pid==-1, then pid=self.curr_proc
@@ -123,6 +149,15 @@ class scheduler:
                 num_active += 1
         return num_active
 
+    def get_ios_in_flight(self, current_time):
+        num_in_flight = 0
+        for pid in range(len(self.proc_info)):
+            for t in self.io_finish_times[pid]:
+                if t > current_time:
+                    num_in_flight += 1
+        return num_in_flight
+
+
     def space(self, num_columns):
         for i in range(num_columns):
             print '%10s' % ' ',
@@ -140,6 +175,11 @@ class scheduler:
         if len(self.proc_info) == 0:
             return
 
+        # track outstanding IOs, per process
+        self.io_finish_times = {}
+        for pid in range(len(self.proc_info)):
+            self.io_finish_times[pid] = []
+
         # make first one active
         self.curr_proc = 0
         self.move_to_running(STATE_READY)
@@ -148,31 +188,58 @@ class scheduler:
         print '%s' % 'Time', 
         for pid in range(len(self.proc_info)):
             print '%10s' % ('PID:%2d' % (pid)),
-
+        print '%10s' % 'CPU',
+        print '%10s' % 'IOs',
         print ''
 
         # init statistics
+        io_busy = 0
         cpu_busy = 0
 
         while self.get_num_active() > 0:
             clock_tick += 1
 
+            # check for io finish
+            io_done = False
+            for pid in range(len(self.proc_info)):
+                if clock_tick in self.io_finish_times[pid]:
+                    # if IO finished, the should do something for related process
+       	            #YOUR CODE
+                    self.io_finish_times[pid].pop(0)
+                    self.move_to_ready(STATE_WAIT, pid)
+                    if self.proc_info[self.curr_proc][PROC_STATE] != STATE_RUNNING:
+                        self.next_proc()
+                    io_done = True
+            
             # if current proc is RUNNING and has an instruction, execute it
-            # statistics clock_tick
             instruction_to_execute = ''
             if self.proc_info[self.curr_proc][PROC_STATE] == STATE_RUNNING and \
                    len(self.proc_info[self.curr_proc][PROC_CODE]) > 0:
+                #pop a instruction from proc_info[self.curr_proc][PROC_CODE]to instruction_to_execute
                 #YOUR CODE
                 instruction_to_execute = self.proc_info[self.curr_proc][PROC_CODE].pop(0)
 
             # OUTPUT: print what everyone is up to
-            print '%3d ' % clock_tick,
+            if io_done:
+                print '%3d*' % clock_tick,
+            else:
+                print '%3d ' % clock_tick,
             for pid in range(len(self.proc_info)):
                 if pid == self.curr_proc and instruction_to_execute != '':
                     print '%10s' % ('RUN:'+instruction_to_execute),
                 else:
                     print '%10s' % (self.proc_info[pid][PROC_STATE]),
-
+            if instruction_to_execute == '':
+                print '%10s' % ' ',
+            else:
+                print '%10s' % 1,
+                cpu_busy += 1
+            num_outstanding = self.get_ios_in_flight(clock_tick)
+            if num_outstanding > 0:
+                print '%10s' % str(num_outstanding),
+                io_busy += 1
+            else:
+                print '%10s' % ' ',
             print ''
 
             # if this is an YIELD instruction, switch to ready state
@@ -181,10 +248,17 @@ class scheduler:
                 #YOUR CODE
                 self.move_to_ready(STATE_RUNNING)
                 self.next_proc()
+            # if this is an IO instruction, switch to waiting state
+            # and add an io completion in the future
+            elif instruction_to_execute == DO_IO:
+                #YOUR CODE
+                self.io_finish_times[self.curr_proc].append(clock_tick + 5)
+                self.move_to_wait(STATE_RUNNING)
+                self.next_proc()
 
             # ENDCASE: check if currently running thing is out of instructions
             self.check_if_done()
-        return (clock_tick)
+        return (cpu_busy, io_busy, clock_tick)
         
 #
 # PARSE ARGUMENTS
@@ -193,19 +267,24 @@ class scheduler:
 parser = OptionParser()
 parser.add_option('-s', '--seed', default=0, help='the random seed', action='store', type='int', dest='seed')
 parser.add_option('-l', '--processlist', default='',
-                  help='a comma-separated list of processes to run, in the form X1:Y1,X2:Y2,... where X is the number of instructions that process should run, and Y the chances (from 0 to 100) that an instruction will use the CPU or issue an YIELD',
+                  help='a comma-separated list of processes to run, in the form X1:Y1:Z1,X2:Y2:Z2,... where X is the number of instructions that process should run, and Y/Z the chances (from 0 to 100) issue an YIELD/IO',
                   action='store', type='string', dest='process_list')
+parser.add_option('-L', '--iolength', default=3, help='how long an IO takes', action='store', type='int', dest='io_length')
 parser.add_option('-p', '--printstats', help='print statistics at end; only useful with -c flag (otherwise stats are not printed)', action='store_true', default=False, dest='print_stats')
 (options, args) = parser.parse_args()
 
 random.seed(options.seed)
 
-s = scheduler()
+process_switch_behavior = SCHED_SWITCH_ON_IO
+io_done_behavior = IO_RUN_LATER
+io_length=options.io_length
+
+
+s = scheduler(process_switch_behavior, io_done_behavior, io_length)
 
 # example process description (10:100,10:100)
 for p in options.process_list.split(','):
     s.load(p)
-
 
 print 'Produce a trace of what would happen when you run these processes:'
 for pid in range(s.get_num_processes()):
@@ -214,11 +293,24 @@ for pid in range(s.get_num_processes()):
         print '  %s' % s.get_instruction(pid, inst)
     print ''
 print 'Important behaviors:'
-print '  System will switch when the current process is FINISHED or ISSUES AN YIELD'
+print '  System will switch when',
+if process_switch_behavior == SCHED_SWITCH_ON_IO:
+    print 'the current process is FINISHED or ISSUES AN YIELD or IO'
+else:
+    print 'error in sched switch on iobehavior'
+    exit (-1)
+print '  After IOs, the process issuing the IO will',
+if io_done_behavior == IO_RUN_LATER:
+    print 'run LATER (when it is its turn)'
+else:
+    print 'error in IO done behavior'
+    exit (-1)
+print ''
 
-(clock_tick) = s.run()
+(cpu_busy, io_busy, clock_tick) = s.run()
 
-if options.print_stats:
-    print ''
-    print 'Stats: Total Time %d' % clock_tick
-    print ''
+print ''
+print 'Stats: Total Time %d' % clock_tick
+print 'Stats: CPU Busy %d (%.2f%%)' % (cpu_busy, 100.0 * float(cpu_busy)/clock_tick)
+print 'Stats: IO Busy  %d (%.2f%%)' % (io_busy, 100.0 * float(io_busy)/clock_tick)
+print ''
